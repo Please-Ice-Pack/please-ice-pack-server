7618129a0f6c412fd6673038d68285eb64cc930b
hocaron <kkannu0407@gmail.com>
2022-08-21 04:44:57 +0900

    feat: 작성자, 수정자 자동 생성 기능 추가

---
+++ src/main/java/com/kurly/pip/config/audit/AuditingConfig.java
@@ -0,0 +1,9 @@
+package com.kurly.pip.config.audit;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {
+}
