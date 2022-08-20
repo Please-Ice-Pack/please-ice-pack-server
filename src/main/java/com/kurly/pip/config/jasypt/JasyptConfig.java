6aba30127a521a4a3c137bab556afc9b2bfa7b8a
hocaron <kkannu0407@gmail.com>
2022-08-21 06:21:56 +0900

    feat: 암호화 설정 추가

---
+++ src/main/java/com/kurly/pip/config/jasypt/JasyptConfig.java
@@ -0,0 +1,11 @@
+package com.kurly.pip.config.jasypt;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
+@Configuration
+@EnableEncryptableProperties
+public class JasyptConfig {
+
+}
