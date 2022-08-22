c57beb56e435bcfb132e7ccbdf8b2f23d799828f
SunwooHo <kkannu0407@gmail.com>
2022-08-23 05:23:16 +0900

    Docs: Swagger 보완 (#27)
    * refactor: 필요한 예외 정의
    
    * refactor: 문서 설명 보완
    
    * refactor: 인증 관련 설명 보완

--- src/main/java/com/kurly/pip/controller/packing/PackingController.java
+++ src/main/java/com/kurly/pip/controller/packing/PackingController.java
@@ -1,7 +1,6 @@
 package com.kurly.pip.controller.packing;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kurly.pip.common.ApiResponse;
-
 import com.kurly.pip.dto.packing.GetPackingResponseDto;
 import com.kurly.pip.dto.packing.PackingResponseDto;
 import com.kurly.pip.dto.packing.UpdatePackingStatusRequestDto;
@@ -25,13 +23,17 @@ public class PackingController {
 
 	private final PackingFacadeService packingFacadeService;
 
-	@ApiOperation("패킹 데이터 조회 API")
+	@ApiOperation(value = "패킹 데이터 조회 API", notes =
+		"orderDetails.coldType: NORMAL(상온) / REFRIGERATED(냉장) / FROZEN(냉동) \n"
+			+ "recognitionResults.coldType: NORMAL(상온) / REFRIGERATED(냉장) / FROZEN(냉동) \n"
+			+ "boxes.type: NORMAL(일반 박스) / POUCH(종이 파우치) / PURPLE(퍼플 박스) \n"
+			+ "refrigerants.type: DRY_ICE(드라이 아이스) / ICE_PACK(아이스팩)")
 	@GetMapping("orders/{orderId}")
 	public ApiResponse<GetPackingResponseDto> getByOrderId(@PathVariable("orderId") Long orderId) {
 		return ApiResponse.success(packingFacadeService.getByOrderId(orderId));
 	}
 
-	@ApiOperation("패킹 상태 변경 API")
+	@ApiOperation(value = "패킹 상태 변경 API", notes = "status: PRE_PROGRESS(패킹 진행 전) / IN_PROGRESS(패킹 진행 중) / DONE(패킹 진행 완료), HOLD(패킹 진행 보류)")
 	@PatchMapping("status/{packingId}")
 	public ApiResponse<PackingResponseDto> updatePackingStatusDone(
 		@PathVariable("packingId") Long packingId,
