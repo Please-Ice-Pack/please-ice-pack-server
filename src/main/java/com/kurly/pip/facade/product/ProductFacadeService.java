84d40bb5b4899cd222ccd5a9819384acb2a43686
hocaron <kkannu0407@gmail.com>
2022-08-22 12:16:24 +0900

    feat: product facade service 생성

---
+++ src/main/java/com/kurly/pip/facade/product/ProductFacadeService.java
@@ -0,0 +1,68 @@
+package com.kurly.pip.facade.product;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.kurly.pip.dto.order.OrderProductResponseDto;
+import com.kurly.pip.dto.order.RecognitionResultResponseDto;
+import com.kurly.pip.entity.order.OrderProduct;
+import com.kurly.pip.entity.order.RecognitionResult;
+import com.kurly.pip.entity.product.Product;
+import com.kurly.pip.service.order.OrderProductService;
+import com.kurly.pip.service.order.RecognitionResultService;
+import com.kurly.pip.service.product.ProductService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProductFacadeService {
+
+    private final OrderProductService orderProductService;
+    private final RecognitionResultService recognitionResultService;
+    private final ProductService productService;
+
+    public List<OrderProductResponseDto> getOrderProductsByOrderId(Long orderId) {
+        List<OrderProduct> orderProducts = orderProductService.getAllByOrderId(orderId);
+        List<Product> products = productService.getByIds(
+            orderProducts
+                .stream()
+                .map(v -> v.getProductId())
+                .collect(Collectors.toList())
+        );
+
+        List<OrderProductResponseDto> orderProductsResponse = null;
+        for (int i = 0; i < products.size(); i++) {
+            orderProductsResponse.add(
+                OrderProductResponseDto.of(
+                    products.get(i),
+                    orderProducts.get(i)
+                ));
+        }
+
+        return orderProductsResponse;
+    }
+
+    public List<RecognitionResultResponseDto> getRecognitionResultsByOrderId(Long orderId) {
+        List<RecognitionResult> recognitionResults = recognitionResultService.getAllByOrderId(orderId);
+        List<Product> products = productService.getByIds(
+            recognitionResults
+                .stream()
+                .map(v -> v.getProductId())
+                .collect(Collectors.toList())
+        );
+
+        List<RecognitionResultResponseDto> recognitionResultsResponse = null;
+        for (int i = 0; i < products.size(); i++) {
+            recognitionResultsResponse.add(
+                RecognitionResultResponseDto.of(
+                    products.get(i),
+                    recognitionResults.get(i)
+                ));
+        }
+
+        return recognitionResultsResponse;
+    }
+}
