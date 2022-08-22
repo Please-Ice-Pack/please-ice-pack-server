5344966a5896cf7ed1d34e04a565ce75022a8d9a
hocaron <kkannu0407@gmail.com>
2022-08-22 16:09:59 +0900

    fix: 토큰이 없는 요청일 경우 필터를 통해 제외

--- src/main/java/com/kurly/pip/config/audit/MemberAuditorAware.java
+++ src/main/java/com/kurly/pip/config/audit/MemberAuditorAware.java
@@ -4,22 +4,19 @@ import java.util.Optional;
 
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-
-import com.kurly.pip.entity.member.Member;
 
 @Component
 class MemberAuditorAware implements AuditorAware<Long> {
 
     @Override
     public Optional<Long> getCurrentAuditor() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        if (authentication == null || !authentication.isAuthenticated()) {
-            return null;
-        }
-
-        return Optional.of(((Member)authentication.getPrincipal()).getId());
+        return Optional.ofNullable(SecurityContextHolder.getContext())
+            .map(SecurityContext::getAuthentication)
+            .filter(authentication -> !authentication.getPrincipal().equals("anonymousUser"))
+            .map(Authentication::getPrincipal)
+            .map(Long.class::cast);
     }
 }
