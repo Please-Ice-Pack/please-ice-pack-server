17a1e0169d0a08861c9756ffb3f9e8e5af9d3b72
hocaron <kkannu0407@gmail.com>
2022-08-24 02:22:38 +0900

    fix: entity 수정

--- src/main/java/com/kurly/pip/entity/packing/PackingBox.java
+++ src/main/java/com/kurly/pip/entity/packing/PackingBox.java
@@ -2,8 +2,11 @@ package com.kurly.pip.entity.packing;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 
 import com.kurly.pip.entity.BaseEntity;
+import com.kurly.pip.entity.box.Type;
 
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -13,12 +16,16 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PackingBox extends BaseEntity {
-    @Column(columnDefinition = "bigint not null")
-    private Long packingId;
+	@Column(columnDefinition = "bigint not null")
+	private Long packingId;
 
-    //TODO: 박스 사이즈 확정되면 enum으로 관리
-    @Column(columnDefinition = "varchar(20) not null")
-    String size;
+	@Enumerated(EnumType.STRING)
+	@Column(columnDefinition = "varchar(20) not null")
+	private Type type;
 
-    private Integer amount;
+	//TODO: 박스 사이즈 확정되면 enum으로 관리
+	@Column(columnDefinition = "varchar(20) not null")
+	String size;
+
+	private Integer amount;
 }
